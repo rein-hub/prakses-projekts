0f0853a4559e5f3187fe4f943aceecfabcd3dfa0
Marckekw <92018388+Marckekw@users.noreply.github.com>
2021-10-27 18:47:34 +0300

    Yet another boring commit to the master branch. This time i messed with application.properties!

--- demo/src/main/resources/data.sql
+++ demo/src/main/resources/data.sql
@@ -2,12 +2,13 @@ DROP TABLE IF EXISTS student;
 
 CREATE TABLE student (
   id INT AUTO_INCREMENT  PRIMARY KEY,
-  first_name VARCHAR(250) NOT NULL,
-  last_name VARCHAR(250) NOT NULL,
-  career VARCHAR(250) DEFAULT NULL
+  name VARCHAR(250) NOT NULL,
+  email VARCHAR(250) NOT NULL,
+  dob INT(250) NOT NULL,
+  age INT(250) NOT NULL
 );
 
-INSERT INTO student (first_name, last_name, career) VALUES
-  ('Aliko', 'Dangote', 'Billionaire Industrialist'),
-  ('Bill', 'Gates', 'Billionaire Tech Entrepreneur'),
-  ('Folrunsho', 'Alakija', 'Billionaire Oil Magnate');
+INSERT INTO student (id, name, email, dob, age) VALUES
+  (1, 'Mario', 'bigdongMario@gmail.com', 2003, 18),
+  (2, 'Marc', 'biggerdongMarc@gmail.com', 2004, 17),
+  (3, 'James', 'biggestdongJames@gmail.com', 2008, 13);
